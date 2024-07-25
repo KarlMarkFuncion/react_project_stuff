4f9bc6388a588d9ca055d356da0263b910eefb24
KarlMarkFuncion <karlmark.funcion@gmail.com>
2024-07-25 22:19:19 +0800

    added SQL connection string

--- quiz_api/quiz_api/obj/Debug/net8.0/quiz_api.AssemblyInfo.cs
+++ quiz_api/quiz_api/obj/Debug/net8.0/quiz_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quiz_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8cb55038bdaa9fbc2766c54f7856a06b1d97f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7214154fdc5fd28e69af596973174c9f66819007")]
 [assembly: System.Reflection.AssemblyProductAttribute("quiz_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quiz_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
